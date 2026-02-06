b03a5cbab652d5c19796acbf17f2201c9f795161
ameer13449-art <ameer13449@gmail.com>
2026-02-06 22:17:11 +0200

    Add basic calculator functionality in main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+print("--- Welcome to Ameer's Pro Calculator ---")
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+op = input("Enter operation (+, -, *, /): ")
+if op == "+":
+ print(num1 + num2)
+elif op == "-":
+ print(num1 - num2)
+elif op == "*":
+ print(num1 * num2)
+elif op == "/":
+ print(num1 / num2)
+print("--------------------------")
+print("Thank you for using my app!")
+print("Done by: Ameer")
